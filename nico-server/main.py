9b578cfc3c348c72f8bedcf0146339a2a329f8f8
jasonbrew-afk <jasonbrew@me.com>
2026-05-24 19:44:45 -0500

    Update nico-server to v0.1.2

--- nico-server/main.py
+++ nico-server/main.py
@@ -1,4 +1,4 @@
-"""Nico Server - FastAPI Webhook Relay (v0.1.1)
+"""Nico Server - FastAPI Webhook Relay (v0.1.2)
 
 Acts as the "eyes" of Nico: receives signals from TradingView
 and relays them to the local `nico-core` engine.
